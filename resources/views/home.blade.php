6c083d80527a36cfe3ab9eaa4d05117749185efe
aep <aepsaprul18@gmail.com>
2024-10-22 16:40:38 +0700

    update dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,7 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Dashboard</h1>
+          <h3 class="m-0" style="font-weight: normal;">Supply Chain Pala</h3>
         </div>
       </div>
     </div>
@@ -40,8 +40,49 @@
   <div class="content">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-3 col-6">
-          tes
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>28.5°C</h3>
+
+              <p>Data Suhu</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-thermometer-half"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>75<sup style="font-size: 20px">%</sup></h3>
+
+              <p>Data Kelembapan</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-tint"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-4 col-6">
+          <!-- small box -->
+          <div class="small-box bg-warning">
+            <div class="inner">
+              <h3>3 Bulan</h3>
+
+              <p>Masa Panen</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-seedling"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
         </div>
       </div>
     </div>
